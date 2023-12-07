8f582ace9dd0c1fefb2e7a82fad3e77112d849f2
proton@google.com <proton@google.com>
2023-12-07 10:42:55 +0800

    新增了随机生成的砖块

--- app.js
+++ app.js
@@ -15,6 +15,34 @@ let groundWidth = 150;
 let xSpeed = 20;
 let ySpeed = 20;
 
+//定义一个防止砖块的空数组
+let brickArray = [];
+
+//设定砖块坐标随机获取函数
+function getRandomARbitiry(min, max) {
+  return min + Math.floor(Math.random() * (max - min));
+}
+
+class Brick {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.width = 30;
+    this.height = 30;
+    this.visible = true;
+    brickArray.push(this);
+  }
+
+  drawBrick() {
+    ctx.fillStyle = "orange";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
+//设定随机画砖块的循环
+for (let i = 0; i <= 10; i++) {
+  new Brick(getRandomARbitiry(0, 950), getRandomARbitiry(0, 450));
+}
 //设定长条移动监听器
 c.addEventListener("mousemove", (e) => {
   ground_x = e.clientX;
@@ -34,12 +62,20 @@ function drawCircle() {
   ctx.fillRect(ground_x, ground_y, groundWidth, groundHeight);
   ctx.strokeRect(ground_x, ground_y, groundWidth, groundHeight);
 
+  //画出砖块
+  brickArray.forEach((brick) => {
+    if (brick.visible) {
+      brick.drawBrick();
+    }
+  });
+
   //画出圆球
   ctx.beginPath();
   ctx.arc(circle_x, circle_y, radius, 0, 2 * Math.PI);
   ctx.stroke();
   ctx.fillStyle = "yellow";
   ctx.fill();
+
   //增加小球碰到长条反弹
   if (
     circle_x >= ground_x - radius &&
