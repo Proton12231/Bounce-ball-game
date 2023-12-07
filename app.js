698aab4feb01409ae4cb7d333076c1f93d69fd77
proton@google.com <proton@google.com>
2023-12-07 10:17:04 +0800

    增加了小球移动

--- app.js
+++ app.js
@@ -7,10 +7,19 @@ let circle_x = 160;
 let circle_y = 60;
 radius = 20;
 
+let xSpeed = 20;
+let ySpeed = 20;
+
 function drawCircle() {
+  //移动球
+  circle_x += xSpeed;
+  circle_y += ySpeed;
+
+  //设定背景刷新
   ctx.fillStyle = "#272727";
   ctx.fillRect(0, 0, canvaswidth, canvasHeight);
 
+  //画出圆球
   ctx.beginPath();
   ctx.arc(circle_x, circle_y, radius, 0, 2 * Math.PI);
   ctx.stroke();
@@ -18,4 +27,4 @@ function drawCircle() {
   ctx.fill();
 }
 
-let game = setInterval(drawCircle(), 25);
+let game = setInterval(drawCircle, 25);
